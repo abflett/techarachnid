9a956de7ccfcf9bc7f9f5e047557e55cf7c9c874
Adam Flett <abflett@yahoo.ca>
2018-01-04 19:10:52 -0600

    cleaned up code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import FullScrDiv from '../components/FullScrDiv'
 import Logo from '../components/hero/logo'
-import './hero/hero.css'
 import Webs from '../components/hero/webs'
 import Lightning from '../components/hero/lightning'
+import './hero/hero.css'
 
 class Hero extends Component {
   constructor() {
@@ -20,11 +20,9 @@ class Hero extends Component {
     const height = window.innerHeight,
           width = window.innerWidth;
     const scale = height / width;
-    
+
     this.setState({height, width, scale});
-    
-    console.log(scale);
-    
+
     if(this.state.scale > 0.72){
       this.setState({divScale: "scale(" + this.state.scale * 2.2 + ")"});
     }else{
@@ -37,12 +35,9 @@ class Hero extends Component {
     this.scrResize();
   }
 
-
   render() {
     return (
-      <div>
           <FullScrDiv>
-
             <div className="shock1">
             </div>
 
@@ -63,9 +58,7 @@ class Hero extends Component {
               
             <div className="layer2">
               <div className="logoDiv">
-
                   <div className="theLightningDiv"><Lightning /></div>
-
               </div>
             </div>
 
@@ -78,7 +71,6 @@ class Hero extends Component {
             </div>
 
           </FullScrDiv>
-      </div>
     );
   }
 }
