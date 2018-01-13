0452bcfe4b11a597b3fc6973f04d7cf0432aacfd
Adam Flett <abflett@yahoo.ca>
2018-01-13 09:54:02 -0600

    unreg server worker.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { unregister } from './registerServiceWorker';
 
 import Home from './pages/home/Home';
+unregister();
+
 
 ReactDOM.render(
     <Home />,
